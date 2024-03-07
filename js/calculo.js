519e737d898829da05e38b7fa1b916ec477b3dff
Gabriel Silva <gabrielsilva96@gmail.com>
2024-03-07 14:59:40 -0300

    corrigido

--- js/calculo.js
+++ js/calculo.js
@@ -1,3 +1,20 @@
+/* O IMC - Indice de Massa corporal é um critério da Organização Mundial da Saúde para dar uma indicação sobre a condição de peso de uma pessoa adulta. 
+Formula do IMC:
+IMC = peso / (altura + altura)
+Elabore um algoritmo que dado o peso e a altura de um adulto mostre sua condição de acordo com a tabela abaixo.
+IMC em adultos. Condição:
+- Abaixo de 18.5 abaixo do peso;
+- Entre 18.5 e 25 peso normal;
+- Entre 25 e 30 acima do peso;
+- Entre 30 e 40 obeso;
+- Acima de 40 obesidade grave; */
+
+/* Nessa const baixei uma biblioteca de elevação "Math.pow" e coloquei a const que precisava ser elevada (altura, 2) a quantidade da elevação, nesse caso 2. "altura elevada a 2", para não precisar fazer "altura * altura". 
+"Math." seria uma classe, por isso está com o M maiúsculo.
+"Pow" seria um método estático.
+Formando Math.pow no código.
+*/
+
 function calcularIMC() {
     var peso = parseFloat(document.getElementById('peso').value);
     var altura = parseFloat(document.getElementById('altura').value);
